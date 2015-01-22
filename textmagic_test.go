5489f355bc96e672a26d0f1a9482890dd1983b38
Michael Woolnough <michael.woolnough@gmail.com>
2015-01-22 19:55:11 +0000

    Added simple invalid command test

--- textmagic_test.go
+++ textmagic_test.go
@@ -3,7 +3,9 @@ package textmagic
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -52,6 +54,17 @@ func TestAccount(t *testing.T) {
 		t.Errorf("test 2: expecting error value of 5, got %d", e.Code)
 	}
 
+}
+
+func TestWrongCommand(t *testing.T) {
+	err := apiConfig.sendAPI("unknown_command", url.Values{}, struct{}{})
+	if err == nil {
+		t.Errorf("expecting error, received nil")
+	} else if e, ok := err.(APIError); !ok {
+		t.Errorf("expecting error of type APIError, got %s: %s", reflect.TypeOf(err).Name, err)
+	} else if e.Code != 3 {
+		t.Errorf("expecting error code 3 (\"Command is undefined\"), got %d (%q)", e.Code, e.Message)
+	}
 }
 
 func TestMain(m *testing.M) {
